f4a9e7134108a702db0e694c44434182f58d1ecd
Nathan Lu <nathanlumax@gmail.com>
2022-05-28 17:28:48 -0500

    added camera view when button is clicked

--- app/src/main/java/me/natelu/truecost/MainActivity.kt
+++ app/src/main/java/me/natelu/truecost/MainActivity.kt
@@ -1,5 +1,8 @@
 package me.natelu.truecost
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -9,28 +12,152 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.firebase.FirebaseApp
+import com.mindorks.paracamera.Camera
 import me.natelu.truecost.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var camera: Camera
+    private val PERMISSION_REQUEST_CODE = 1
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Create Saved Instance State
         super.onCreate(savedInstanceState)
 
+        // Initialize Firebase
+        FirebaseApp.initializeApp(this)
+
+        // Initialize Camera
+        camera = Camera.Builder()
+            .resetToCorrectOrientation(true)
+            .setTakePhotoRequestCode(Camera.REQUEST_TAKE_PHOTO)
+            .setDirectory("pics")
+            .setName("cost_${System.currentTimeMillis()}")
+            .setImageFormat(Camera.IMAGE_JPEG)
+            .setCompression(75)
+            .build(this)
+
+        // Initialize Binding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Initialize Toolbar
         setSupportActionBar(binding.toolbar)
 
+        // Initialize NavController
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
+        // Initialize Camera Bar
         binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            // Take Picture
+            camera.takePicture()
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
+        }
+    }
+
+    fun takePicture(view: View) {
+        if (!hasPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) ||
+            !hasPermission(android.Manifest.permission.CAMERA)
+        ) {
+            // If do not have permissions then request it
+            requestPermissions()
+        } else {
+            // else all permissions granted, go ahead and take a picture using camera
+            try {
+                camera.takePicture()
+            } catch (e: Exception) {
+                // Show a toast for exception
+                Toast.makeText(
+                    this.applicationContext, getString(R.string.error_taking_picture),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    private fun requestPermissions() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+        ) {
+            Toast.makeText(
+                this.applicationContext, getString(R.string.permission_storage_rationale),
+                Toast.LENGTH_LONG
+            ).show()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    android.Manifest.permission.CAMERA
+                ), PERMISSION_REQUEST_CODE
+            )
+            return
+        }
+    }
+
+    private fun hasPermission(permission: String): Boolean {
+        return ActivityCompat.checkSelfPermission(
+            this,
+            permission
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray
+    ) {
+        when (requestCode) {
+            PERMISSION_REQUEST_CODE -> {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty()
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED
+                ) {
+                    try {
+                        camera.takePicture()
+                    } catch (e: Exception) {
+                        Toast.makeText(
+                            this.applicationContext, getString(R.string.error_taking_picture),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+                return
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == Camera.REQUEST_TAKE_PHOTO) {
+                val bitmap = camera.cameraBitmap
+                if (bitmap != null) {
+                    // Do something with bitmap
+
+
+//                    imageView.setImageBitmap(bitmap)
+//                    detectDeliciousFoodOnDevice(bitmap)
+                } else {
+                    Toast.makeText(
+                        this.applicationContext, getString(R.string.picture_not_taken),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
         }
     }
 
